82010e81985cf4a848780c33e5822392303a4906
sharanya1803 <sharanyak43@gmail.com>
2026-01-29 12:44:07 +0530

    Update message

--- src/components/navbar/hero/hero.jsx
+++ src/components/navbar/hero/hero.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import './hero.css'
 import profile_img from "../../../assets/profile_img.jpeg";
+import resume from "../../../assets/myresume.odt";
+
 
 const hero = () => {
     return (
         <div id='home' className='hero'>
-            <img src={profile_img} alt=""className="profile-img" />
+            <img src={profile_img} alt="" className="profile-img" />
             <h1><span>I'am Sharanya Kumari</span></h1>
             <p>I am a motivated student who loves learning new technologies and building responsive, user-friendly applications</p>
             <div className="hero-action">
                 <div className='hero-connect'>
                     Connect with me
                 </div>
-                <div className="hero-resume"> My Resume</div>
+                <a
+                    href={resume}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ textDecoration: "none", color: "inherit" }}
+                >
+                    <div className="hero-resume"> My Resume</div></a>
             </div>
         </div>
 
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useRef } from "react";
 import './navbar.css'
 import logo from '../../assets/logo.jpg'
-
+import toggle from '../../assets/toggle.png'
+import closemenu from '../../assets/closemenu.png'
 const navbar = () => {
-    
+    const menuRef = useRef();
+
+    const openMenu = () => {
+        menuRef.current.style.right = "0";
+    }
+    const closeMenu = () => {
+        menuRef.current.style.right = "-350px";
+    }
+
+
     return (
         <div className='navbar'>
             <img src={logo} alt="" />
-            <ul className="navme">
+            <img src={toggle} onClick={openMenu} alt="" className='nav-open' />
+
+            <ul ref={menuRef} className="navme">
+                <img src={closemenu} onClick={closeMenu} alt="" className='nav-close' />
                 <li><p>Home</p></li>
                 <li ><p>About me</p></li>
                 <li ><p>projects</p></li>
